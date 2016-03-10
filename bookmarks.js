1ecb1426f1c6953092178c65437a8a24317f5c96
Gabor Szabo <gabor@szabgab.com>
2016-03-10 22:56:40 +0200

    routing and editor template

--- bookmarks.js
+++ bookmarks.js
@@ -1,5 +1,33 @@
-angular.module('BookmarksApp', [])
-.controller('BookmarksController', function($scope) {
-    $scope.message = "Hello World";
+angular.module('BookmarksApp', ['ngRoute'])
+.config(function($logProvider){
+    $logProvider.debugEnabled(true);
+})
+.config(['$routeProvider', function($routeProvider) {
+   $routeProvider.
+       when('/', {
+   	       templateUrl: 'main.html',
+       }).
+       when('/editor', {
+           templateUrl: 'editor.html',
+       }).
+       otherwise({
+           redirectTo: '/'
+       });
+}])
+.controller('BookmarksController', function($scope, $log, $location) {
+    $scope.open_editor = function() {
+        $log.debug('open_editor'); 
+		$scope.editor = {};
+        $location.path('/editor');
+	};
+
+	$scope.cancel = function() {
+        $location.path('/');
+	};
+	$scope.save = function() {
+		$log.debug('save');
+		$log.debug($scope.editor.url);
+        $location.path('/');
+	};
 });
  
